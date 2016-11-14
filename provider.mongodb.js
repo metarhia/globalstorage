4895b351a9430beee80a765cccdbb96c0e5d6811
Mykola Bilochub <nbelochub@gmail.com>
2016-11-14 16:14:58 +0200

    Rename sparse to nullable

--- provider.mongodb.js
+++ provider.mongodb.js
@@ -183,7 +183,7 @@ MongodbProvider.prototype.index = function(def, callback) {
   });
   var options = {
     unique: def.unique !== undefined ? def.unique : false,
-    sparse: def.sparse !== undefined ? def.sparse : false,
+    sparse: def.nullable !== undefined ? def.nullable : false,
     background: def.background !== undefined ? def.background : true
   };
   category.createIndex(keys, options, callback);
