fff30a9e7d3668ba608f4de46ac1e9be1be12178
tshemsedinov <timur.shemsedinov@gmail.com>
2017-07-24 13:19:07 +0300

    Refactor exports and internal modules

--- gs.js
+++ gs.js
@@ -1,228 +1,202 @@
 'use strict';
 
-const api = {};
+const util = require('util');
 
-api.gs = {};
-api.fs = require('fs');
-api.util = require('util');
-api.events = require('events');
-api.crypto = require('crypto');
-api.mkdirp = require('mkdirp');
+const constants = require('./lib/constants');
+const infrastructure = require('./lib/infrastructure');
+const transformations = require('./lib/transformations');
 
-api.jstp = require('metarhia-jstp');
-api.common = require('metarhia-common');
-api.metasync = require('metasync');
+const StorageProvider = require('./lib/provider');
 
-require('./lib/provider')(api);
-const gs = new api.gs.StorageProvider();
-api.gs = gs;
-module.exports = gs;
+const FsProvider = require('./lib/provider.fs');
+const MemoryProvider = require('./lib/provider.memory');
+const MongodbProvider = require('./lib/provider.mongodb');
 
-[ 'provider', 'provider.fs', 'provider.memory', 'provider.mongodb',
-  'cursor', 'cursor.fs', 'cursor.memory', 'cursor.mongodb',
-  'connection', 'category', 'transformations'
-].forEach((name) => require('./lib/' + name)(api));
+const FsCursor = require('./lib/cursor.fs');
+const MemoryCursor = require('./lib/cursor.memory');
+const MongodbCursor = require('./lib/cursor.mongodb');
 
-api.gs.NO_STORAGE = 'No storage provider available';
-api.gs.NOT_IMPLEMENTED = 'Not implemented';
+const Category = require('./lib/category');
+const Connection = require('./lib/connection');
 
-// Hash keyed by provider name
-//
-gs.providers = {
-  fs: gs.FsProvider,
-  memory: gs.MemoryProvider,
-  mongodb: gs.MongodbProvider,
-};
+function GlobalStorage() {
+  this.providers = {
+    fs: FsProvider,
+    memory: MemoryProvider,
+    mongodb: MongodbProvider
+  };
 
-// Objects cache keyed by id
-//
-gs.memory = new gs.MemoryProvider();
+  this.memory = new MemoryProvider();
+  this.active = false;
+  this.offline = true;
+  this.local = null;
+  this.infrastructure = {};
+  this.infrastructureTree = {};
+  this.infrastructureIndex = [];
+  this.infrastructureMask = 0;
+  this.connections = {};
+  this.nextId = 0;
+  this.categories = {};
 
-// Database mode
-//   closed - no one provider is available
-//   offline - local storage provider is available
-//   online - local and connections are available
-//
-gs.mode = 'closed';
+  this.constants = constants;
+  this.infrastructure = infrastructure;
+  this.transformations = transformations;
 
-// Local storage provider
-// implementing interface globalStorageProvider
-//
-gs.local = null;
+  this.FsCursor = FsCursor;
+  this.MemoryCursor = MemoryCursor;
+  this.MongodbCursor = MongodbCursor;
+}
 
-// Database opened
-//
-gs.active = false;
+util.inherits(GlobalStorage, StorageProvider);
 
-gs.open = (options, callback) => {
-  gs.memory.open({ gs: options.gs }, () => {});
-  const Provider = gs.providers[options.provider];
+GlobalStorage.prototype.open = function(
+  // Open database
+  options, // options
+  callback // callback
+) {
+  this.memory.open({ gs: options.gs }, () => {});
+  const Provider = this.providers[options.provider];
   if (Provider) {
-    gs.local = new Provider();
-    gs.active = true;
-    gs.local.open(options, callback);
+    this.local = new Provider();
+    this.active = true;
+    this.local.open(options, callback);
   } else {
-    callback(new Error(api.gs.NO_STORAGE));
+    callback(new Error(constants.NO_STORAGE));
   }
 };
 
-// Remote storage provider
-// implementing interface globalStorageProvider
-//
-gs.connections = {};
-
-gs.connect = (
+GlobalStorage.prototype.connect = function(
   // Connect to Global Storage server
   options, // connection parammeters
   // Example: { url: 'gs://user:password@host:port/database' }
   callback // on connect function(err, connection)
-) => {
-  const connection = new gs.Connection(options);
+) {
+  const connection = new Connection(options);
   callback(null, connection);
 };
 
-// Categories cache keyed by category name
-//
-gs.categories = {};
-
-gs.category = (
+GlobalStorage.prototype.category = function(
   // Get Category
   name, // name of category
   callback // function(err, category)
-) => {
-  let cat = gs.categories[name];
+) {
+  let cat = this.categories[name];
   if (!cat) {
-    cat = new gs.Category(name);
-    gs.categories[name] = cat;
+    cat = new Category(name);
+    this.categories[name] = cat;
   }
   callback(null, cat);
 };
 
-gs.get = (id, callback) => {
-  gs.memoryStorageProvider.get(id, (err, data) => {
+GlobalStorage.prototype.get = function(
+  // Get object by id
+  id, // object id
+  callback // function(err, object)
+) {
+  this.memoryStorageProvider.get(id, (err, data) => {
     if (data) callback(null, data);
     else get(id, callback);
   });
 
   function get(id, callback) {
-    if (gs.local) {
-      gs.local.get(id, (err, data) => {
+    if (this.local) {
+      this.local.get(id, (err, data) => {
         if (!err) return callback(null, data);
-        const sid = gs.findServer(id);
-        const connection = gs.infrastructure.index[sid];
+        const sid = this.findServer(id);
+        const connection = this.infrastructure.index[sid];
         connection.get(id, callback);
       });
     } else if (callback) {
-      callback(new Error(api.gs.NO_STORAGE));
+      callback(new Error(constants.NO_STORAGE));
     }
   }
 };
 
-gs.create = (obj, callback) => {
-  if (gs.local) {
-    gs.local.create(obj, callback);
+GlobalStorage.prototype.create = function(
+  // Create new object
+  obj, // object
+  callback // function(err, id)
+) {
+  if (this.local) {
+    this.local.create(obj, callback);
   } else if (callback) {
-    callback(new Error(api.gs.NO_STORAGE));
+    callback(new Error(constants.NO_STORAGE));
   }
 };
 
-gs.update = (obj, callback) => {
-  if (gs.local) {
-    gs.local.update(obj, callback);
+GlobalStorage.prototype.update = function(
+  // Update Object
+  obj, // object
+  callback // function(err)
+) {
+  if (this.local) {
+    this.local.update(obj, callback);
   } else if (callback) {
-    callback(new Error(api.gs.NO_STORAGE));
+    callback(new Error(constants.NO_STORAGE));
   }
 };
 
-gs.delete = (id, callback) => {
-  if (gs.local) {
-    gs.local.delete(id, callback);
+GlobalStorage.prototype.delete = function(
+  // Delete object
+  id, // object id
+  callback // function(err)
+) {
+  if (this.local) {
+    this.local.delete(id, callback);
   } else if (callback) {
-    callback(new Error(api.gs.NO_STORAGE));
+    callback(new Error(constants.NO_STORAGE));
   }
 };
 
-gs.select = (query, options, callback) => {
-  if (gs.local) {
-    return gs.local.select(query, options, callback);
+GlobalStorage.prototype.select = function(
+  // Select dataset
+  query, // object
+  options, // object
+  callback // function(err, cursor)
+) {
+  if (this.local) {
+    return this.local.select(query, options, callback);
   } else if (callback) {
-    callback(new Error(api.gs.NO_STORAGE));
+    callback(new Error(constants.NO_STORAGE));
   }
 };
 
-gs.index = (def, callback) => {
-  if (gs.local) {
-    gs.local.index(def, callback);
+GlobalStorage.prototype.index = function(
+  // Create index
+  def, // declarative definotion
+  callback // function(err)
+) {
+  if (this.local) {
+    this.local.index(def, callback);
   } else if (callback) {
-    callback(new Error(api.gs.NO_STORAGE));
+    callback(new Error(constants.NO_STORAGE));
   }
 };
 
-// Global Storage Infrastructure
-//
-gs.infrastructure = {};
-
-// Servers tree
-//
-gs.infrastructure.tree = {};
-
-// Servers index
-//
-gs.infrastructure.index = [];
-
-// Server bit mask
-//
-gs.infrastructure.mask = 0;
-
-// Build index array from tree
-//
-function buildIndex(tree) {
-  const result = [];
-  const parseTree = (index, depth, node) => {
-    const isBranch = !!node[0];
-    if (isBranch) {
-      parseTree(index, depth + 1, node[0]);
-      parseTree(index + (1 << depth), depth + 1, node[1]);
-    } else {
-      result[index] = node;
-    }
-  };
-  parseTree(0, 0, tree);
-
-  const height = Math.ceil(Math.log(result.length) / Math.log(2));
-  let i, j, depth;
-  for (i = result.length; i >= 0; i--) {
-    depth = Math.ceil(Math.log(i + 1) / Math.log(2));
-    for (j = 1; result[i] && j < 1 << height - depth; j++) {
-      if (!result[i + (j << depth)]) {
-        result[i + (j << depth)] = result[i];
-      }
-    }
-  }
-  return result;
-}
-
-// Assign new infrastructure tree
-//
-gs.infrastructure.assign = (tree) => {
-  gs.infrastructure.servers = tree;
-  const index = buildIndex(tree);
-  gs.infrastructure.index = index;
-  gs.infrastructure.bits = Math.log(index.length) / Math.log(2);
-  gs.infrastructure.mask = Math.pow(2, gs.infrastructure.bits) - 1;
+GlobalStorage.prototype.infrastructureAssign = function(
+  // Assign new infrastructure tree
+  tree // new infrastructure tree
+) {
+  this.infrastructure.servers = tree;
+  const index = infrastructure.buildIndex(tree);
+  this.infrastructure.index = index;
+  this.infrastructure.bits = Math.log(index.length) / Math.log(2);
+  this.infrastructure.mask = Math.pow(2, this.infrastructure.bits) - 1;
 };
 
-// Get server for id
-//
-gs.findServer = (id) => (
-  gs.infrastructure.index[id & gs.infrastructure.mask]
-);
+GlobalStorage.prototype.findServer = function(
+  // Get server for id
+  id // object id
+) {
+  const prefix = id & this.infrastructure.mask;
+  return this.infrastructure.index[prefix];
+};
 
-// Last id in storage
-//
-gs.nextId = 0;
+GlobalStorage.prototype.generateId = function(
+  // Get server for id
+  // This function not used now
+) {
+  return this.nextId++;
+};
 
-// Get server for id
-//
-gs.generateId = () => gs.nextId++;
-// This function not used now
+module.exports = new GlobalStorage();
