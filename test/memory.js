c2a648da09ca0a180ed7dedc46382bd42911af95
tshemsedinov <timur.shemsedinov@gmail.com>
2018-03-26 00:49:16 +0300

    Refactor tests for Mongodb storage provider
    PR-URL: https://github.com/metarhia/globalstorage/pull/109

--- test/memory.js
+++ test/memory.js
@@ -1,52 +1,47 @@
 'use strict';
 
-module.exports = (api) => {
+const gs = require('..');
 
-  const gs = api.gs;
+const ds1 = [ { id: 1 }, { id: 2 } ];
+const ds2 = [ { id: 2 }, { id: 3 } ];
 
-  const ds1 = [ { id: 1 }, { id: 2 } ];
-  const ds2 = [ { id: 2 }, { id: 3 } ];
+console.dir({
+  union: gs.transformations.union(ds1, ds2),
+  intersection: gs.transformations.intersection(ds1, ds2),
+  difference: gs.transformations.difference(ds1, ds2),
+  complement: gs.transformations.complement(ds1, ds2)
+});
 
-  console.dir({
-    union: gs.transformations.union(ds1, ds2),
-    intersection: gs.transformations.intersection(ds1, ds2),
-    difference: gs.transformations.difference(ds1, ds2),
-    complement: gs.transformations.complement(ds1, ds2)
+const mc1 = new gs.MemoryCursor(ds1);
+const mc2 = mc1.clone();
+
+mc1.dataset[0].name = 'qwerty';
+console.dir(mc1.dataset);
+console.dir(mc2.dataset);
+
+mc1.clone()
+  .order('id')
+  .fetch((err, data) => {
+    console.dir({ order: data });
   });
 
-  const mc1 = new gs.MemoryCursor(ds1);
-  const mc2 = mc1.clone();
+mc1.clone()
+  .desc(['id', 'name'])
+  .fetch((err, data) => {
+    console.dir({ desc: data });
+  });
 
-  mc1.dataset[0].name = 'qwerty';
-  console.dir(mc1.dataset);
-  console.dir(mc2.dataset);
+const persons = [
+  { name: 'Marcus Aurelius', city: 'Rome', born: 121 },
+  { name: 'Victor Glushkov', city: 'Rostov on Don', born: 1923 },
+  { name: 'Ibn Arabi', city: 'Murcia', born: 1165 },
+  { name: 'Mao Zedong', city: 'Shaoshan', born: 1893 },
+  { name: 'Rene Descartes', city: 'La Haye en Touraine', born: 1596 }
+];
 
-  mc1.clone()
-    .order('id')
-    .fetch((err, data) => {
-      console.dir({ order: data });
-    });
-
-  mc1.clone()
-    .desc(['id', 'name'])
-    .fetch((err, data) => {
-      console.dir({ desc: data });
-    });
-
-  const persons = [
-    { name: 'Marcus Aurelius', city: 'Rome', born: 121 },
-    { name: 'Victor Glushkov', city: 'Rostov on Don', born: 1923 },
-    { name: 'Ibn Arabi', city: 'Murcia', born: 1165 },
-    { name: 'Mao Zedong', city: 'Shaoshan', born: 1893 },
-    { name: 'Rene Descartes', city: 'La Haye en Touraine', born: 1596 }
-  ];
-
-  const mcPersons = new gs.MemoryCursor(persons);
-  mcPersons
-    .select({ born: '< 1500' })
-    .order('born')
-    .fetch((err, data) => {
-      console.dir({ selected: data });
-    });
-
-};
+const mcPersons = new gs.MemoryCursor(persons);
+mcPersons.select({ born: '< 1500' })
+  .order('born')
+  .fetch((err, data) => {
+    console.dir({ selected: data });
+  });
