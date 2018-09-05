83104a88b3b12e6d932effb17d288a8db561ea16
tshemsedinov <timur.shemsedinov@gmail.com>
2018-09-06 01:36:53 +0300

    Change case style in entity fields naming
    PR-URL: https://github.com/metarhia/globalstorage/pull/207

--- test/memory.js
+++ test/memory.js
@@ -3,21 +3,21 @@
 const gs = require('..');
 const metaschema = require('metaschema');
 
-const ds1 = [ { id: 1, name: 'qwerty' }, { id: 2 } ];
-const ds2 = [ { id: 2 }, { id: 3 } ];
+const ds1 = [ { Id: 1, Name: 'qwerty' }, { Id: 2 } ];
+const ds2 = [ { Id: 2 }, { Id: 3 } ];
 
 api.metatests.test('dataset operation', test => {
   const union = gs.transformations.union(ds1, ds2);
-  const unionExpected = [{ id: 1, name: 'qwerty' }, { id: 2 }, { id: 3 }];
+  const unionExpected = [{ Id: 1, Name: 'qwerty' }, { Id: 2 }, { Id: 3 }];
   test.strictSame(union, unionExpected, 'union should have all ids');
   const inter = gs.transformations.intersection(ds1, ds2);
-  const interExpected = [{ id: 2 }];
+  const interExpected = [{ Id: 2 }];
   test.strictSame(inter, interExpected, 'intersection should have only id 2');
   const diff = gs.transformations.difference(ds1, ds2);
-  const diffExpected = [{ id: 1, name: 'qwerty' }];
+  const diffExpected = [{ Id: 1, Name: 'qwerty' }];
   test.strictSame(diff, diffExpected, 'diffirence should have only id 1');
   const comp = gs.transformations.complement(ds1, ds2);
-  const compExpected = [{ id: 3 }];
+  const compExpected = [{ Id: 3 }];
   test.strictSame(comp, compExpected, 'complement should have only id 3');
   test.end('operation tests done');
 });
@@ -25,9 +25,9 @@ api.metatests.test('dataset operation', test => {
 api.metatests.test('datasets tests', test => {
   const mc1 = new gs.MemoryCursor(ds1, () => {});
   const mc2 = mc1.clone();
-  const ds1Expected = [{ id: 1, name: 'qwerty' }, { id: 2 }];
+  const ds1Expected = [{ Id: 1, Name: 'qwerty' }, { Id: 2 }];
   test.strictSame(mc1.dataset, ds1Expected, 'Dataset 1 should be changed');
-  const ds2Expected = [{ id: 1, name: 'qwerty' }, { id: 2 }];
+  const ds2Expected = [{ Id: 1, Name: 'qwerty' }, { Id: 2 }];
   test.strictSame(mc2.dataset, ds2Expected, 'Dataset 2 should be the same');
   test.end('datasets tests done');
 });
@@ -35,10 +35,10 @@ api.metatests.test('datasets tests', test => {
 api.metatests.test('sort order', test => {
   const mc = new gs.MemoryCursor(ds1, () => {});
   mc.clone()
-    .order('id')
+    .order('Id')
     .fetch((err, data) => {
       if (err) return test.throws(err, 'test order 1');
-      const expected = [{ id: 1, name: 'qwerty' }, { id: 2 }];
+      const expected = [{ Id: 1, Name: 'qwerty' }, { Id: 2 }];
       test.strictSame(data, expected, 'Wrong data');
       test.end('test order 1 done');
     });
@@ -47,9 +47,9 @@ api.metatests.test('sort order', test => {
 api.metatests.test('sort order desc', test => {
   const mc = new gs.MemoryCursor(ds1, () => {});
   mc.clone()
-    .desc(['id', 'name'])
+    .desc(['Id', 'Name'])
     .fetch((err, data) => {
-      const expected = [{ id: 2 }, { id: 1, name: 'qwerty' }];
+      const expected = [{ Id: 2 }, { Id: 1, Name: 'qwerty' }];
       test.strictSame(data, expected, 'Wrong data');
       test.end('test order 2 done');
     });
@@ -57,19 +57,19 @@ api.metatests.test('sort order desc', test => {
 
 api.metatests.test('cursor select', test => {
   const persons = [
-    { name: 'Marcus Aurelius', city: 'Rome', born: 121 },
-    { name: 'Victor Glushkov', city: 'Rostov on Don', born: 1923 },
-    { name: 'Ibn Arabi', city: 'Murcia', born: 1165 },
-    { name: 'Mao Zedong', city: 'Shaoshan', born: 1893 },
-    { name: 'Rene Descartes', city: 'La Haye en Touraine', born: 1596 }
+    { Id: 1, Name: 'Marcus Aurelius', City: 'Rome', Born: 121 },
+    { Id: 2, Name: 'Victor Glushkov', City: 'Rostov on Don', Born: 1923 },
+    { Id: 3, Name: 'Ibn Arabi', City: 'Murcia', Born: 1165 },
+    { Id: 4, Name: 'Mao Zedong', City: 'Shaoshan', Born: 1893 },
+    { Id: 5, Name: 'Rene Descartes', City: 'La Haye en Touraine', Born: 1596 }
   ];
   const mcPersons = new gs.MemoryCursor(persons);
-  mcPersons.select({ born: '< 1500' })
-    .order('born')
+  mcPersons.select({ Born: '< 1500' })
+    .order('Born')
     .fetch((err, data) => {
       const expected = [
-        { name: 'Ibn Arabi', city: 'Murcia', born: 1165 },
-        { name: 'Marcus Aurelius', city: 'Rome', born: 121 },
+        { Id: 3, Name: 'Ibn Arabi', City: 'Murcia', Born: 1165 },
+        { Id: 1, Name: 'Marcus Aurelius', City: 'Rome', Born: 121 }
       ];
       test.strictSame(data, expected, 'Wrong data');
       test.end('select test done');
@@ -78,9 +78,9 @@ api.metatests.test('cursor select', test => {
 
 api.metatests.test('cursor schema', test => {
   const languages = [
-    { Name: 'English', Locale: 'en' },
-    { Name: 'Ukrainian', Locale: 'uk' },
-    { Name: 'Russian', Locale: 'ru' }
+    { Id: 1, Name: 'English', Locale: 'en' },
+    { Id: 2, Name: 'Ukrainian', Locale: 'uk' },
+    { Id: 3, Name: 'Russian', Locale: 'ru' }
   ];
   metaschema.load('schemas/system', (err, schemas) => {
     if (err) throw err;
@@ -99,11 +99,11 @@ api.metatests.test('cursor schema', test => {
 
 api.metatests.test('cursor union', test => {
   const languages1 = [
-    { id: 1, Name: 'English', Locale: 'en' }
+    { Id: 1, Name: 'English', Locale: 'en' }
   ];
   const languages2 = [
-    { id: 2, Name: 'Ukrainian', Locale: 'uk' },
-    { id: 3, Name: 'Russian', Locale: 'ru' }
+    { Id: 2, Name: 'Ukrainian', Locale: 'uk' },
+    { Id: 3, Name: 'Russian', Locale: 'ru' }
   ];
 
   const mcLanguages1 = new gs.MemoryCursor(languages1);
@@ -120,12 +120,12 @@ api.metatests.test('cursor union', test => {
 
 api.metatests.test('cursor intersection', test => {
   const languages1 = [
-    { id: 1, Name: 'English', Locale: 'en' },
-    { id: 2, Name: 'Russian', Locale: 'ru' }
+    { Id: 1, Name: 'English', Locale: 'en' },
+    { Id: 2, Name: 'Russian', Locale: 'ru' }
   ];
   const languages2 = [
-    { id: 3, Name: 'Ukrainian', Locale: 'uk' },
-    { id: 2, Name: 'Russian', Locale: 'ru' }
+    { Id: 3, Name: 'Ukrainian', Locale: 'uk' },
+    { Id: 2, Name: 'Russian', Locale: 'ru' }
   ];
 
   const mcLanguages1 = new gs.MemoryCursor(languages1);
