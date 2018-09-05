684f5d6e97ce654777f5eb17d2abe10971e5eac5
tshemsedinov <timur.shemsedinov@gmail.com>
2018-09-06 02:48:12 +0300

    Temporary workaround for metaschema bug
    Refs: https://github.com/metarhia/metaschema/issues/101

--- test/system.js
+++ test/system.js
@@ -7,15 +7,17 @@ metaschema.load('schemas/system', (err, schema) => {
   metaschema.build(schema);
 
   for (const schema of metaschema.categories.values()) {
-    console.log('Category: ' + schema.name);
-    const { definition } = metaschema.categories.get(schema.name);
-    for (const name in definition) {
-      const field = definition[name];
-      console.log('  Field: ' + name);
-      const decorator = field.constructor.name;
-      if (decorator !== 'Object') {
-        console.log('    Decorator: ' + decorator);
-        console.log('    Definition: ' + JSON.stringify(field));
+    if (schema.name) {
+      console.log('Category: ' + schema.name);
+      const { definition } = metaschema.categories.get(schema.name);
+      for (const name in definition) {
+        const field = definition[name];
+        console.log('  Field: ' + name);
+        const decorator = field.constructor.name;
+        if (decorator !== 'Object') {
+          console.log('    Decorator: ' + decorator);
+          console.log('    Definition: ' + JSON.stringify(field));
+        }
       }
     }
     console.log();
