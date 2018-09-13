0c138b6e2c8bc54ce0b362fb6788f40daf597eb1
tshemsedinov <timur.shemsedinov@gmail.com>
2018-09-13 23:35:05 +0300

    Remove schema iteration and output
    PR-URL: https://github.com/metarhia/globalstorage/pull/223

--- test/system.js
+++ test/system.js
@@ -5,22 +5,4 @@ const metaschema = require('metaschema');
 metaschema.load('schemas/system', (err, schema) => {
   if (err) throw err;
   metaschema.build(schema);
-
-  for (const schema of metaschema.categories.values()) {
-    if (schema.name) {
-      console.log('Category: ' + schema.name);
-      const { definition } = metaschema.categories.get(schema.name);
-      for (const name in definition) {
-        const field = definition[name];
-        console.log('  Field: ' + name);
-        const decorator = field.constructor.name;
-        if (decorator !== 'Object') {
-          console.log('    Decorator: ' + decorator);
-          console.log('    Definition: ' + JSON.stringify(field));
-        }
-      }
-    }
-    console.log();
-  }
-
 });
