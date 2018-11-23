a38f9e674c68b4908fda866b87d636408f524b46
Dmytro Nechai <nechaido@gmail.com>
2018-11-23 18:58:34 +0200

    Use link name for destination property in many-to-many tables
    PR-URL: https://github.com/metarhia/globalstorage/pull/282

--- test/fixtures/ddl/many.js
+++ test/fixtures/ddl/many.js
@@ -33,14 +33,14 @@ ALTER TABLE "Schema2" ADD CONSTRAINT "pkSchema2Id" PRIMARY KEY ("Id");
 
 CREATE TABLE "Schema2link" (
   "Schema2" bigint NOT NULL,
-  "Schema1" bigint NOT NULL
+  "link"    bigint NOT NULL
 );
 ALTER TABLE "Schema2link" ADD CONSTRAINT "fkSchema2linkSchema2" \
 FOREIGN KEY ("Schema2") REFERENCES "Schema2" ("Id") \
 ON UPDATE RESTRICT ON DELETE CASCADE;
-ALTER TABLE "Schema2link" ADD CONSTRAINT "fkSchema2linkSchema1" \
-FOREIGN KEY ("Schema1") REFERENCES "Schema1" ("Id") \
+ALTER TABLE "Schema2link" ADD CONSTRAINT "fkSchema2linklink" \
+FOREIGN KEY ("link") REFERENCES "Schema1" ("Id") \
 ON UPDATE RESTRICT ON DELETE CASCADE;
 ALTER TABLE "Schema2link" ADD CONSTRAINT "pklinkSchema2Schema1" \
-PRIMARY KEY ("Schema2", "Schema1");`,
+PRIMARY KEY ("Schema2", "link");`,
 });
