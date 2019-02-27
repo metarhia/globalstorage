8828cf8d90ca9d34dfb3664b7e6ea4de16d0e696
Dmytro Nechai <nechaido@gmail.com>
2019-02-27 16:51:42 +0200

    Prohibit detail categories from having hierarchical division
    PR-URL: https://github.com/metarhia/globalstorage/pull/328

---
+++ test/ms.config.detail-with-hierarchy.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const metaschema = require('metaschema');
+const metatests = require('metatests');
+
+const { options, config } = require('../lib/metaschema-config/config');
+
+const { SchemaValidationError, MetaschemaError } = metaschema.errors;
+
+metatests.test('Fully supports schemas/system', async test => {
+  try {
+    await metaschema.fs.load(
+      'test/fixtures/detail-with-hierarchy',
+      options,
+      config
+    );
+  } catch (error) {
+    test.strictSame(
+      error,
+      new MetaschemaError([
+        new SchemaValidationError('detailHierarchy', 'Detail', {
+          type: 'Subdivision',
+          master: 'Master',
+        }),
+        new SchemaValidationError('detailHierarchy', 'Included', {
+          type: 'Catalog',
+          master: 'Master',
+        }),
+      ])
+    );
+    test.end();
+    return;
+  }
+
+  test.fail('Expected to fail');
+  test.end();
+});
