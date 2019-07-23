044ebce805c62d99596f71fb811e224ae79ab04a
Rumiantsev Oleksii <rumiantsev.oleksii@gmail.com>
2019-07-23 17:54:33 +0300

    fixup! Add linter config for schemas

---
+++ schemas-eslint-config.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = {
+  globals: {
+    api: false,
+    Action: false,
+    Many: false,
+    Master: false,
+    Validate: false,
+    Unique: false,
+    Index: false,
+    System: false,
+    Global: false,
+    Registry: false,
+    Local: false,
+    Dictionary: false,
+    Log: false,
+    Enum: false,
+    Flags: false,
+    Catalog: false,
+    Subsystem: false,
+    Form: false,
+  },
+  rules: {
+    strict: 'off',
+    'new-cap': 'off',
+  },
+};
