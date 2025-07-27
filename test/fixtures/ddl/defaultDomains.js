b559c53fed75fe108fa1dd15fa8a7cd2177b91ec
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2025-07-27 03:01:36 +0300

    Remove old version codebase

--- test/fixtures/ddl/defaultDomains.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = () => ({
-  name: 'generateDDL with default domains',
-  schemas: [],
-  // SQL for default Metaschema domains will be added by test runner
-  expectedSql: '',
-});
---
+++ test/gs.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const test = require('node:test');
+const assert = require('node:assert');
+const gs = require('..');
+
+test('Stub', () => {
+  assert.ok(gs);
+});
