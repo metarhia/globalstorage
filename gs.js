633449912e9f70edb21044dca4b3019d285c4168
Volodymyr Rusynov <rearchontis@gmail.com>
2026-02-11 17:44:19 +0200

    Initial FTS implementation

--- gs.js
+++ gs.js
@@ -5,5 +5,13 @@ const contract = require('./lib/contract.js');
 const crdt = require('./lib/crdt.js');
 const keys = require('./lib/keys.js');
 const storage = require('./lib/storage.js');
+const fts = require('./lib/fts/search-index.js');
 
-module.exports = { ...chain, ...contract, ...crdt, ...keys, ...storage };
+module.exports = {
+  ...chain,
+  ...contract,
+  ...crdt,
+  ...keys,
+  ...storage,
+  ...fts,
+};
