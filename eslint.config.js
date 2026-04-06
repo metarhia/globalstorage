9fbce5e7c29c2769fc516950393a53dcb0a9ac80
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2026-04-07 00:40:22 +0300

    Remove /dist/ folder

--- eslint.config.js
+++ eslint.config.js
@@ -2,13 +2,4 @@
 
 const init = require('eslint-config-metarhia');
 
-module.exports = [
-  { ignores: ['globalstorage.mjs'] },
-  ...init,
-  {
-    files: ['dist/**/*.js'],
-    languageOptions: {
-      sourceType: 'module',
-    },
-  },
-];
+module.exports = [{ ignores: ['globalstorage.mjs'] }, ...init];
